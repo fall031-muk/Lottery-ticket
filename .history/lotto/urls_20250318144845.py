58f723472d503143e731dc885a621e5f68b0774d
fall031-muk <fall900802@gmail.com>
2025-03-19 15:11:25 +0900

    모든 히스토리 파일 삭제 및 정적--> 동적으로 코드 수정

--- .history/lotto/urls_20250318144845.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from lotto.api.views import GetLottoNumber, LottoMainView
-
-urlpatterns = [
-    path('', LottoMainView.as_view(), name='lotto_main'),  # 메인 페이지
-    path('numbers', GetLottoNumber.as_view(), name='get_lotto'),  # API 엔드포인트
-]
