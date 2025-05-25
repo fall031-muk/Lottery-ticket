8fdfcf648f811f7110651f9b260cf3c64daa1c47
fall031-muk <fall900802@gmail.com>
2025-05-25 16:56:02 +0900

    feat: 개인정보 처리방침 및 이용약관 페이지 추가, 메인 페이지 푸터에 링크 추가

--- lotto/urls.py
+++ lotto/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from lotto.api.views import GetLottoNumber, LottoMainView, GetLatestDrawNumber, GetDrawInfo
 from lotto import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', LottoMainView.as_view(), name='lotto_main'),  # 메인 페이지
@@ -8,4 +9,6 @@ urlpatterns = [
     path('latest-draw', GetLatestDrawNumber.as_view(), name='get_latest_draw'),  # 최신 회차 API
     path('draw-info', GetDrawInfo.as_view(), name='get_draw_info'),  # 특정 회차 정보 API
     path('about/', views.about, name='about'),  # 사이트 소개 페이지
+    path('privacy/', TemplateView.as_view(template_name='lotto/privacy.html'), name='privacy'),  # 개인정보 처리방침
+    path('terms/', TemplateView.as_view(template_name='lotto/terms.html'), name='terms'),  # 이용약관
 ]
